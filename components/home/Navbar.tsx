d93ed3faec907c0555514c4322006c92329ac3c8
mujahidjuel3 <mujahidjuel056@gmail.com>
2026-01-10 17:08:58 +0600

    Sticky navbar is done like figma.

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -6,22 +6,22 @@ export default function NavbarTailwind() {
   return (
     <header className="fixed top-[39px] left-0 right-0 z-[9999] flex justify-center">
       <nav className="navbar-container flex items-center rounded-[24px]">
-        <div className="navbar-content flex items-center justify-center">
-          <Link href="/" className="navbar-link text-center transition-colors">
+        <div className="navbar-content flex items-center justify-center font-semibold">
+          <Link href="/" className="navbar-link text-center transition-colors font-semibold">
             Home
           </Link>
-          <Link href="/service" className="navbar-link text-center transition-colors">
+          <Link href="/service" className="navbar-link text-center transition-colors font-semibold">
             Service
           </Link>
-          <Link href="/" className="webkarigor-button relative inline-flex items-center justify-center transition-colors">
+          <Link href="/" className="webkarigor-button relative inline-flex items-center justify-center transition-colors font-semibold">
             <span className="webkarigor-inner w-full h-full flex items-center justify-center">
-              <span className="webkarigor-text text-center">Webkarigor</span>
+              <span className="webkarigor-text text-center font-semibold">Webkarigor</span>
             </span>
           </Link>
-          <Link href="/case" className="navbar-link text-center transition-colors">
+          <Link href="/case" className="navbar-link text-center transition-colors font-semibold">
             Case
           </Link>
-          <Link href="/about-us" className="navbar-link text-center transition-colors">
+          <Link href="/about-us" className="navbar-link text-center transition-colors font-semibold">
             About us
           </Link>
         </div>
